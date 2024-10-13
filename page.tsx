5e21e667b48f6028ca1d9f917af45a2b0223788f
Kinza-125 <ka8553200@gmail.com>
2024-10-13 00:02:16 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import Links from "./components/link"
+export default function Homepage (){
+  return(
+    <div>
+      <Links/>
+      <h1 className="green">welcome to the Home page</h1>
+    </div>
+  )
+}
